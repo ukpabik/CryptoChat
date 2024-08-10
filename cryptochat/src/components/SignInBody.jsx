f1f3ba3fdba1c33d29d7b4786d98b66d858446d5
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-10 13:01:33 -0400

    Start creating login page
    No functionality yet

--- cryptochat/src/components/SignInBody.jsx
+++ cryptochat/src/components/SignInBody.jsx
@@ -2,7 +2,21 @@ function SignInBody(){
   return(
     <body>
       <div class = "flexbody">
+        <div class = "login-box">
 
+          <form action = "" method = "get" class = "login-form">
+            <div class = "login-field">
+              <label for= "username">Enter Username: </label>
+              <input type = "text" class = "login-username" id = "name" required />
+              <label for= "username">Enter Username: </label>
+              <input type = "text" class = "login-username" id = "name" required />
+              </div>
+            <div class = "submit-button">
+              <input class = "submit-button" type = "submit" value = "Sign In" />
+            </div>
+          </form>
+          
+        </div>
 
       </div>
     </body>
