bf55c1877b67af523a6c2a71130ec259e1c891ff
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-14 10:28:08 -0400

    Format time layout

--- cryptochat/client/src/components/HomepageBody.jsx
+++ cryptochat/client/src/components/HomepageBody.jsx
@@ -145,29 +145,13 @@ function HomepageBody(){
   const formatTimeForDisplay = (timeSent) => {
     
     const date = new Date(timeSent);
+    const localTime = date.toLocaleString(); 
+    const [day, time] = localTime.split(',');
+    const [space, currentTime, period] = time.split(' ');
+
+    const [hours, minutes, seconds] = currentTime.split(':')
   
-    
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-  
-    let hours = date.getHours();
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-  
-    
-    let period = 'AM';
-    if (hours >= 12) {
-      period = 'PM';
-      if (hours > 12) hours -= 12;
-    } else if (hours === 0) {
-      hours = 12;
-    }
-  
-    
-    const formattedTime = `${hours}:${minutes} ${period}`;
-  
-    
-    return `${month}/${day}/${year} ${formattedTime}`;
+    return `${day} ${hours}:${minutes} ${period}`;
   };
 
 
--- cryptochat/server/server.js
+++ cryptochat/server/server.js
@@ -16,11 +16,13 @@ import pkg from 'pg';
 //CONNECTING TO POSTGRESQL
 const { Pool } = pkg;
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
+  // host: process.env.PG_HOST,
+  // port: process.env.PG_PORT,
+  // database: process.env.PG_DATABASE,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 pool.connect()
   .then(() => console.log('Connected to PostgreSQL'))
