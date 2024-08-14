00b971c0c31e3e3c9be2a5578d37eeb61626bdcb
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-13 22:19:39 -0400

    Update HomepageBody.jsx

--- cryptochat/client/src/components/HomepageBody.jsx
+++ cryptochat/client/src/components/HomepageBody.jsx
@@ -151,7 +151,7 @@ function HomepageBody(){
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
   
-    let hours = date.getHours() + 4;
+    let hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
   
     
