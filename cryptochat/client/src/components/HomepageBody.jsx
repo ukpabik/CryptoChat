0fd382597fcc9b192505136a557de68b9c4b115e
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-14 22:46:09 -0400

    Add websocket as a fallback

--- cryptochat/client/src/components/HomepageBody.jsx
+++ cryptochat/client/src/components/HomepageBody.jsx
@@ -41,7 +41,7 @@ function HomepageBody(){
       auth: {
         username: username || 'Guest',
         serverOffset: 0,
-        transports: ['polling'],
+        transports: ['polling', 'websocket'],
       },
       
     })
