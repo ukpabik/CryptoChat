8e3273446c49d0758b721bb587a4de0ec31f94ee
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-14 22:40:15 -0400

    Update routes

--- cryptochat/client/src/App.jsx
+++ cryptochat/client/src/App.jsx
@@ -35,9 +35,9 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path="" element={<Homepage />} />
-            <Route path="signin" element={<SignIn />} />
-            <Route path="about" element={<About />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </main>
         <Footer />
