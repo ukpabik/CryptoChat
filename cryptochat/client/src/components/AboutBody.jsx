7a8b610e6f552d0c59f3598edcd7ea1b528b49b6
Kelechi Ukpabi <kuolingou@gmail.com>
2024-08-19 10:07:43 -0400

    Update styling for about and signin page

--- cryptochat/client/src/components/AboutBody.jsx
+++ cryptochat/client/src/components/AboutBody.jsx
@@ -8,7 +8,7 @@ function AboutBody(){
         <div className = "aboutme-text-container">
           <h1 className='aboutme-title'>About CryptoChat</h1>
           <p className='aboutme-text'>
-          Welcome to CryptoChat! I’m a college student with a strong interest in technology and digital currencies. I created CryptoChat because I’m passionate about cryptocurrency and wanted a space where people can come together to talk about it. Whether you’re here to chat about the latest market trends, explore new developments, or just share ideas with others who love crypto, I hope you find CryptoChat to be a welcoming and engaging place. Let’s explore the world of cryptocurrency together!
+          Welcome to CryptoChat! I’m a college student with a strong interest in technology and digital currencies. I created CryptoChat because I’m passionate about cryptocurrency and wanted a space where people can come together to talk about it. Whether you’re here to chat about the latest market trends, explore new developments, or just share ideas with others who love crypto, I hope you find CryptoChat to be a welcoming and engaging place.
           </p>
           <p className='aboutme-text'>
             CryptoChat is more than a personal project;
@@ -17,7 +17,7 @@ function AboutBody(){
             I built CryptoChat to make it easier for everyone, from beginners to seasoned 
             enthusiasts, to connect and explore the dynamic world of crypto. 
             I hope you find it as exciting and useful as I do. 
-            Thanks for being a part of this journey with me!
+            Thanks for being a part of this journey with me! Let’s explore the world of cryptocurrency together!
           </p>
         </div>
 
